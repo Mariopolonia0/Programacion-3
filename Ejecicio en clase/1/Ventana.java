63da76a86b5b053a0cc50c6bf95be131cd6c86a9
Mario Peña <65502311+Mariopolonia0@users.noreply.github.com>
2022-04-06 18:01:52 -0400

    Add files via upload

---
+++ Ejecicio en clase/1/Ventana.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Ventana{
+	public static void main(String[] args) {
+		JFrame f=new JFrame();
+		f.setSize(400,500);
+		f.setTitle("Soy tu papi");
+		f.setVisible(true);
+
+		FileDialog d = new FileDialog(f,"File Dialog");
+		d.setSize(200,300);
+		d.setTitle("MMG");
+		d.setVisible(true);
+	}
+}
